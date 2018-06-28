42deb41823f8adda74809a9a6ad995b08fe7047b
Michael Ballard <ballard.michael9@gmail.com>
2018-06-28 18:02:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 class BoardMember {
   
-  constructor(name, homeState, training)
+  constructor(name, homeState, training){
+    this.name = name 
+    this.homeState = homeState
+    this.training = training
+  }
   
 }
