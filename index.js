39de241415d053c3fb6088973cd412796a6413a7
Michael Ballard <ballard.michael9@gmail.com>
2018-06-28 18:10:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+
+class BoardMember {
+  
+  constructor(name, homeState, training){
+    this.name = name 
+    this.homeState = homeState
+    this.training = training
+  }
+
+ // I do not have to define function with instance methods
+  veto() {
+    return "No, I must disagree"
+  }
+  
+   approve() {
+    return "You can do that!"
+  }
+  
+  doCharity(){
+    return "I like to help people."
+  }
+  
+  releasePressStatement(){
+    return "You will see great things from Scuber."
+  }
+  
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+  
+}
+
+class Ceo extends BoardMember {
+  
+  hireEmployee(){
+    return "Welcome aboard!"
+  }
+  
+}
